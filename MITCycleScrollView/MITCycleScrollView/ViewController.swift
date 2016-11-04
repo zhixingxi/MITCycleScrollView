f0515b9a786d634b0ffece867312e885c5370b57
MQL-IT <mengqingling@MQLing.local>
2016-11-04 14:53:41 +0800

    第一次提交

---
+++ MITCycleScrollView/MITCycleScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MITCycleScrollView
+//
+//  Created by MQL-IT on 2016/11/4.
+//  Copyright © 2016年 MIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
